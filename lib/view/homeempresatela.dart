211a8fc61fe362d4be434695320da04689aa6b5c
Elio Antonio Paulino Junior <elio_paulino@hotmail.com>
2021-04-12 20:23:33 -0300

    Commit

--- lib/view/homeempresatela.dart
+++ lib/view/homeempresatela.dart
@@ -14,11 +14,7 @@ class _HomeEmpresaTelaState extends State<HomeEmpresaTela> {
         centerTitle: true,
       ),
       drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
         child: ListView(
-          // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
           children: <Widget>[
             DrawerHeader(
